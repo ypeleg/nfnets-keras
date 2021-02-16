3c82e3380af7518b0f3cfa1952ad34736de2e66b
Prateek Kumar Jain <pikku.ga@gmail.com>
2021-02-16 13:33:08 +0530

    fixed error ValueError: Tensor-typed variable initializers must either be wrapped in an init_scope...

--- nfnets_keras/nfnet_layers.py
+++ nfnets_keras/nfnet_layers.py
@@ -16,7 +16,7 @@ class WSConv2D(Conv2D):
         mean = tf.math.reduce_mean(weight, axis = (0, 1, 2), keepdims = True)
         var = tf.math.reduce_variance(weight, axis = (0, 1, 2), keepdims = True)
         fan_in = np.prod(weight.shape[:-1])
-        gain = self.add_weight(name = 'gain', shape = (weight.shape[-1],), initializer = "ones", trainable = True, dtype = self.dtype)
+        gain = self.add_weight(name = 'gain', shape = (weight.shape[-1],), initializer = lambda : tf.keras.initializers.Ones, trainable = True, dtype = self.dtype)
         scale = tf.math.rsqrt(tf.math.maximum(var * fan_in, tf.convert_to_tensor(eps, dtype = self.dtype))) * gain
         return weight * scale - (mean * scale)
 
